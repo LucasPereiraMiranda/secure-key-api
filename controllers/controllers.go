43708009ff72eb57a7a735279546aee79672b7f7
Lucas Pereira Miranda <lucas.pereira.miranda01@gmail.com>
2024-04-04 17:20:55 -0300

    feat: add base health method

---
+++ controllers/controllers.go
@@ -0,0 +1,22 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func Health(w http.ResponseWriter, r *http.Request) {
+	response := map[string]string{"status": "healthy"}
+
+	responseJSON, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, "Error on encode response", http.StatusInternalServerError)
+		return
+	}
+	w.Write(responseJSON)
+}
+
+func GeneratePassword(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Start Page")
+}
