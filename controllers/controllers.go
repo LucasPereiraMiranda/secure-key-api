f85838ffffbc2335a12d3264ec7aa9d5b00ea459
Lucas Pereira Miranda <lucas.pereira.miranda01@gmail.com>
2024-05-02 18:48:48 -0300

    feat: create contracts

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -2,9 +2,23 @@ package controllers
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 )
+
+type GeneratePasswordRequest struct {
+	NumDigits      int  `json:"num_digits"`
+	HasSpecialKeys bool `json:"has_special_keys"`
+	HasNumbersKey  bool `json:"has_numbers_key"`
+	UpperCaseKey   bool `json:"upper_case_key"`
+}
+
+type PasswordResponse struct {
+	Password string `json:"password"`
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
 
 func Health(w http.ResponseWriter, r *http.Request) {
 	response := map[string]string{"status": "healthy"}
@@ -14,8 +28,6 @@ func Health(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error on encode response", http.StatusInternalServerError)
 		return
 	}
+	w.Header().Set("Content-Type", "application/json")
 	w.Write(responseJSON)
-}
-func GeneratePassword(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Start Page")
 }
